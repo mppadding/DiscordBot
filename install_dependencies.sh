a1d69609ab67f6d4a286513f24c5e1261e5d0387
Matthijs Padding <mppadding@gmail.com>
2016-12-29 10:21:16 +0000

    Created a file to more easily install dependencies for the bot

---
+++ install_dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install discord.js for the bot
+npm install discord.js
+
+# Install node-json-db for a database framework. Required for some commands
+npm install node-json-db
+
+# Install request to easily make http(s) requests
+npm install request
+
+# Install striptags. Required for some commands
+npm install striptags
